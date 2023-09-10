68ffa2620650cf15831e78012214d01fe32d0972
francollovera <francollovera@gmail.com>
2023-09-10 15:28:55 -0300

    a ver

--- carrito.js
+++ carrito.js
@@ -110,89 +110,89 @@ function comprarCarrito(){
 
 }
 
-// REPLACE WITH YOUR PUBLIC KEY AVAILABLE IN: https://developers.mercadopago.com/panel
-const mercadopago = new MercadoPago('APP_USR-5102c4a1-b24f-4aed-80d0-29d6a0a2ac3f', {
-    locale: '<LOCALE>' // The most common are: 'pt-BR', 'es-AR' and 'en-US'
-  });
+// // REPLACE WITH YOUR PUBLIC KEY AVAILABLE IN: https://developers.mercadopago.com/panel
+// const mercadopago = new MercadoPago('APP_USR-5102c4a1-b24f-4aed-80d0-29d6a0a2ac3f', {
+//     locale: '<LOCALE>' // The most common are: 'pt-BR', 'es-AR' and 'en-US'
+//   });
   
-  // Handle call to backend and generate preference.
-  document.getElementById("checkout-btn").addEventListener("click", function () {
+//   // Handle call to backend and generate preference.
+//   document.getElementById("checkout-btn").addEventListener("click", function () {
   
-    $('#checkout-btn').attr("disabled", true);
+//     $('#checkout-btn').attr("disabled", true);
   
-    const orderData = {
-      quantity: document.getElementById("quantity").value,
-      description: document.getElementById("product-description").innerHTML,
-      price: document.getElementById("unit-price").innerHTML
-    };
+//     const orderData = {
+//       quantity: document.getElementById("quantity").value,
+//       description: document.getElementById("product-description").innerHTML,
+//       price: document.getElementById("unit-price").innerHTML
+//     };
   
-    fetch("http://localhost:8080/create_preference", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(orderData),
-    })
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (preference) {
-        createCheckoutButton(preference.id);
+//     fetch("http://localhost:8080/create_preference", {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify(orderData),
+//     })
+//       .then(function (response) {
+//         return response.json();
+//       })
+//       .then(function (preference) {
+//         createCheckoutButton(preference.id);
   
-        $(".shopping-cart").fadeOut(500);
-        setTimeout(() => {
-          $(".container_payment").show(500).fadeIn();
-        }, 500);
-      })
-      .catch(function () {
-        alert("Unexpected error");
-        $('#checkout-btn').attr("disabled", false);
-      });
-  });
+//         $(".shopping-cart").fadeOut(500);
+//         setTimeout(() => {
+//           $(".container_payment").show(500).fadeIn();
+//         }, 500);
+//       })
+//       .catch(function () {
+//         alert("Unexpected error");
+//         $('#checkout-btn').attr("disabled", false);
+//       });
+//   });
   
-  function createCheckoutButton(preferenceId) {
-    // Initialize the checkout
-    const bricksBuilder = mercadopago.bricks();
+//   function createCheckoutButton(preferenceId) {
+//     // Initialize the checkout
+//     const bricksBuilder = mercadopago.bricks();
   
-    const renderComponent = async (bricksBuilder) => {
-      if (window.checkoutButton) window.checkoutButton.unmount();
-      await bricksBuilder.create(
-        'wallet',
-        'button-checkout', // class/id where the payment button will be displayed
-        {
-          initialization: {
-            preferenceId: preferenceId
-          },
-          callbacks: {
-            onError: (error) => console.error(error),
-            onReady: () => {}
-          }
-        }
-      );
-    };
-    window.checkoutButton =  renderComponent(bricksBuilder);
-  }
+//     const renderComponent = async (bricksBuilder) => {
+//       if (window.checkoutButton) window.checkoutButton.unmount();
+//       await bricksBuilder.create(
+//         'wallet',
+//         'button-checkout', // class/id where the payment button will be displayed
+//         {
+//           initialization: {
+//             preferenceId: preferenceId
+//           },
+//           callbacks: {
+//             onError: (error) => console.error(error),
+//             onReady: () => {}
+//           }
+//         }
+//       );
+//     };
+//     window.checkoutButton =  renderComponent(bricksBuilder);
+//   }
   
-  // Handle price update
-  function updatePrice() {
-    let quantity = document.getElementById("quantity").value;
-    let unitPrice = document.getElementById("unit-price").innerHTML;
-    let amount = parseInt(unitPrice) * parseInt(quantity);
+//   // Handle price update
+//   function updatePrice() {
+//     let quantity = document.getElementById("quantity").value;
+//     let unitPrice = document.getElementById("unit-price").innerHTML;
+//     let amount = parseInt(unitPrice) * parseInt(quantity);
   
-    document.getElementById("cart-total").innerHTML = "$ " + amount;
-    document.getElementById("summary-price").innerHTML = "$ " + unitPrice;
-    document.getElementById("summary-quantity").innerHTML = quantity;
-    document.getElementById("summary-total").innerHTML = "$ " + amount;
-  }
+//     document.getElementById("cart-total").innerHTML = "$ " + amount;
+//     document.getElementById("summary-price").innerHTML = "$ " + unitPrice;
+//     document.getElementById("summary-quantity").innerHTML = quantity;
+//     document.getElementById("summary-total").innerHTML = "$ " + amount;
+//   }
   
-  document.getElementById("quantity").addEventListener("change", updatePrice);
-  updatePrice();
+//   document.getElementById("quantity").addEventListener("change", updatePrice);
+//   updatePrice();
   
-  // Go back
-  document.getElementById("go-back").addEventListener("click", function () {
-    $(".container_payment").fadeOut(500);
-    setTimeout(() => {
-      $(".shopping-cart").show(500).fadeIn();
-    }, 500);
-    $('#checkout-btn').attr("disabled", false);
-  });
+//   // Go back
+//   document.getElementById("go-back").addEventListener("click", function () {
+//     $(".container_payment").fadeOut(500);
+//     setTimeout(() => {
+//       $(".shopping-cart").show(500).fadeIn();
+//     }, 500);
+//     $('#checkout-btn').attr("disabled", false);
+//   });
