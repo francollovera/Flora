63f32923bf3a2b6f6e10bb034f48026ed6288fe3
francollovera <francollovera@gmail.com>
2025-01-09 15:37:38 -0300

    proyectonuevo

--- carrito.js
+++ carrito.js
@@ -96,7 +96,10 @@ function actualizarTotal(){
 total.innerText = `$${totalCalculado}`;
 }
 
+botonComprar.addEventListener("click", comprarCarrito);
+
 botonComprar.addEventListener("click", vaciarCarrito)
+
 function comprarCarrito(){
     productosEnCarrito.length = 0;
     localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito))
